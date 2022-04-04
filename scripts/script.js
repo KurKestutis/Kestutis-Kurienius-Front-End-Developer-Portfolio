4f4531de384c49634ba9753e38832b7303aee43a
Kestutis Kurienius <kurkestutis@gmail.com>
2022-04-04 12:56:52 +0300

    Cookies message added and auto remove function created.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
+"use strict";
 
+const cookiesMessage = document.querySelector(".cookies");
+const cookiesMessageTimmer = document.querySelector(".cokies__text__seconds");
+
+let time = 10;
+const showCookiesMessage = () => {
+  if (time > 1) {
+    time--;
+    console.log(time);
+    cookiesMessageTimmer.textContent = `${time}`;
+  } else {
+    cookiesMessage.remove();
+  }
+};
+
+showCookiesMessage(time);
+setInterval(showCookiesMessage, 1000);
