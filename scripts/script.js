a5b9160509682e5a0e29af87e7b43a7369469ba8
Kestutis Kurienius <kurkestutis@gmail.com>
2022-04-04 15:16:51 +0300

    Cookies message fade out and button close implemented.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,25 @@
 "use strict";
 
+window.onbeforeunload = () => {
+  window.scrollTo(0, 0);
+};
+
+const intro = document.querySelector(".intro");
 const cookiesMessage = document.querySelector(".cookies");
 const cookiesMessageTimmer = document.querySelector(".cokies__text__seconds");
+const btnCloseCookie = document.querySelector(".btn--close-cookies");
 
-let time = 10;
+setTimeout(() => {
+  intro.remove();
+}, 2000);
+
+btnCloseCookie.addEventListener("click", function () {
+  cookiesMessage.remove();
+});
+
+let time = 8;
 const showCookiesMessage = () => {
-  if (time > 1) {
+  if (time >= 1) {
     time--;
     console.log(time);
     cookiesMessageTimmer.textContent = `${time}`;
@@ -16,7 +30,3 @@ const showCookiesMessage = () => {
 
 showCookiesMessage(time);
 setInterval(showCookiesMessage, 1000);
-
-window.onbeforeunload = () => {
-  window.scrollTo(0, 0);
-};
