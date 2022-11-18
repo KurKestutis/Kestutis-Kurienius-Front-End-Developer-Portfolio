a1c1a0450d71656fc8db5ee973276979bac79194
Kestutis Kurienius <kurkestutis@gmail.com>
2022-11-18 17:52:36 +0200

    Cookies message deleted.

--- scripts/script.js
+++ scripts/script.js
@@ -3,29 +3,3 @@
 window.onbeforeunload = () => {
   window.scrollTo(0, 0);
 };
-
-const intro = document.querySelector(".intro");
-const cookiesMessage = document.querySelector(".cookies");
-const cookiesMessageTimmer = document.querySelector(".cookies__text__seconds");
-const btnCloseCookie = document.querySelector(".cookies__text__btn");
-
-setTimeout(() => {
-  intro.remove();
-}, 2000);
-
-btnCloseCookie.addEventListener("click", function () {
-  cookiesMessage.remove();
-});
-
-let time = 8;
-const showCookiesMessage = () => {
-  if (time >= 1) {
-    cookiesMessageTimmer.textContent = `${time}`;
-    time--;
-  } else {
-    cookiesMessage.remove();
-  }
-};
-
-showCookiesMessage(time);
-setInterval(showCookiesMessage, 1000);
